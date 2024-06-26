a9dcc46d3f781471f8ec363f275743c76c425fbe
Ishmum Jawad Khan <ishmum.khan@dsinnovators.com>
2024-06-26 18:05:04 +0600

    Add shortcuts for zen-mode, comment and surround

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,12 +2,21 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local comment = require("Comment.api")
 local map = vim.keymap.set
 
 map({ "n", "v" }, "f", function()
   require("flash").jump()
 end, { desc = "Flash" })
 
+map("n", "<C-c>", comment.toggle.linewise.current, { desc = "Toggle Comment" })
+map(
+  "x",
+  "<C-c>",
+  comment.call("toggle.blockwise", "g@"),
+  { desc = "Toggle Comment Block", expr = true }
+)
+map("n", "<C-w>", "<cmd>bd<cr>", { desc = "Close current buffer" })
 map("n", "<S-u>", "<cmd>redo<cr>", { desc = "Redo last undo" })
 map("n", "<C-q>", "<cmd>only<cr><cmd>exit<cr>", { desc = "Exit" })
 map("n", "m", "<cmd>messages<cr>", { desc = "Show messages" })
@@ -23,3 +32,6 @@ map(
   "<cmd>Neotree close<cr><cmd>silent only<cr>",
   { desc = "Close other windows" }
 )
+map("n", "<C-z>", function()
+  require("zen-mode").toggle()
+end)
